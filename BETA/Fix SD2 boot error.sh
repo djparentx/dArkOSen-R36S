90b8f3994353532f977c03803ff2f81210bc5649
djparentx <276067176+djparentx@users.noreply.github.com>
2026-07-20 16:26:04 -0600

    Delete BETA/Fix SD2 boot error.sh

--- BETA/Fix SD2 boot error.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo sed -i 's|^LABEL=EASYROMS[[:space:]]\+/roms[[:space:]]\+exfat|/dev/mmcblk0p3 /roms exfat|' /etc/fstab
-
-sudo sed -i '/\/opt\/system\/Tools none nofail,x-systemd\.device-timeout=7,bind/ {
-    / 0 0[[:space:]]*$/! s/$/ 0 0/
-}' /etc/fstab
