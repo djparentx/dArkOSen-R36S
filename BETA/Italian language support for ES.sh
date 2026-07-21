66adbd4da314f432f2150bd45d2e7da630946c8f
djparentx <276067176+djparentx@users.noreply.github.com>
2026-07-21 13:45:32 -0600

    Add files via upload

---
+++ BETA/Italian language support for ES.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE="https://raw.githubusercontent.com/djparentx/dArkOSen-R36S/main/usr/bin/emulationstation/resources/locale/it/emulationstation2.po"
+DEST_DIR="/usr/bin/emulationstation/resources/locale/it"
+DEST_FILE="$DEST_DIR/emulationstation2.po"
+
+mkdir -p "$DEST_DIR"
+
+wget -q -O "$DEST_FILE" "$SOURCE"
+
+if [[ $? -eq 0 ]]; then
+    chmod 644 "$DEST_FILE"
+    echo "Installed Italian locale."
+else
+    echo "Failed to download Italian locale."
+    exit 1
+fi
